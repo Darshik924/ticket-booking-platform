37f3a9fbd540dd36c3874322a40ed215c1a306a0
Darshik924 <darshikladhe182@gmail.com>
2026-06-11 16:12:54 +0530

    Added CRUD Apis for Events+Seats

--- backend/src/routes/eventRoutes.ts
+++ backend/src/routes/eventRoutes.ts
@@ -6,6 +6,7 @@ import {
   updateAnEvent,
   deleteAnEvent,
 } from "../controllers/eventController";
+import { getSeatMap } from "../controllers/seatController";
 
 const eventRouter = express.Router();
 // Get all the events for ther users
@@ -16,7 +17,9 @@ eventRouter.get("/:id", getEvent);
 eventRouter.post("/", createAnEvent);
 eventRouter.put("/:id", updateAnEvent);
 eventRouter.delete("/:id", deleteAnEvent);
-// So here we will need an admin middleware and stuff which i have not put for now since we will need to hardCode that later and also in the database need to make an admin account for ther CRUD ops of the Events + Seats 
+// So here we will need an admin middleware and stuff which i have not put for now since we will need to hardCode that later and also in the database need to make an admin account for ther CRUD ops of the Events + Seats
 // Date Format need to provide is 2026-06-11T14:30:00.000Z
+
+eventRouter.get("/:id/seats", getSeatMap);
 
 export default eventRouter;
