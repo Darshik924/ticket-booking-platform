a226a887f3e32aa6614858aba5781047854554d4
Harshal Karanje <karanjeharshal007@gmail.com>
2026-06-11 12:33:38 +0530

    Implemented a new feature for the authentication

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,8 +1,11 @@
 // changes User model
-
+// This file contains the authentication controller functions for handling user registration, login, and profile retrieval. It uses the authService functions to perform the necessary operations and sends appropriate HTTP responses based on the outcomes.
 import { Request, Response } from "express";
 import { registerUser } from "../services/authService";
+import { loginUser } from "../services/authService";
+import { AuthRequest } from "../middlewares/authMiddleware";
 
+// Controller function to handle user registration
 export const register = async (
   req: Request,
   res: Response
@@ -33,4 +36,48 @@ export const register = async (
       message: error.message,
     });
   }
+};
+
+
+// Controller function to handle user login
+export const login = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required",
+      });
+    }
+
+    const data = await loginUser(
+      email,
+      password
+    );
+
+    return res.status(200).json({
+      success: true,
+      data,
+    });
+  } catch (error: any) {
+    return res.status(401).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// Controller function to get the authenticated user's profile
+export const getProfile = async (
+  req: AuthRequest,
+  res: Response
+) => {
+  res.status(200).json({
+    success: true,
+    user: req.user,
+  });
 };
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { register } from "../controllers/authController";
+import { register, login } from "../controllers/authController";
+import { authenticate } from "../middlewares/authMiddleware";
+import { getProfile } from "../controllers/authController";
+
 
 const router = Router();
 
+//register and login routes
 router.post("/register", register);
+
+router.post("/login", login);
+
+router.get("/me", authenticate, getProfile);
 
 export default router;
