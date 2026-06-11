f5b3c81aaa831374a950d1a0c4ab0e33ea2e9e83
Darshik924 <darshikladhe182@gmail.com>
2026-06-11 21:50:26 +0530

    Added CRUD Apis for Events+Seats

--- backend/src/routes/eventRoutes.ts
+++ backend/src/routes/eventRoutes.ts
@@ -19,6 +19,7 @@ eventRouter.put("/:id", updateAnEvent);
 eventRouter.delete("/:id", deleteAnEvent);
 // So here we will need an admin middleware and stuff which i have not put for now since we will need to hardCode that later and also in the database need to make an admin account for ther CRUD ops of the Events + Seats
 // Date Format need to provide is 2026-06-11T14:30:00.000Z
+// Other than that we have The Logical part of the Events + Seats Done
 
 eventRouter.get("/:id/seats", getSeatMap);
 
