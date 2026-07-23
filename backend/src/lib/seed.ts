eb2eab429d757d70101c2db12e60e918a93d9d60
naresh-coder123 <nc9676@gmail.com>
2026-07-23 16:04:13 +0530

    changed seed.ts directory

--- backend/src/lib/seed.ts
+++ backend/src/lib/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../src/generated/prisma/index.js";
+import { PrismaClient } from "../generated/prisma/index.js";
 
 const prisma = new PrismaClient();
 
@@ -94,3 +94,5 @@ main()
   .finally(async () => {
     await prisma.$disconnect();
   });
+
+
